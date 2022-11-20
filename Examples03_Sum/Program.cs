c4490b914a0e7bd5bbf0c37322bb36210681bbb3
warlordsxp <warlordsxp@yandex.ru>
2022-11-20 11:54:21 +0300

    Add in Examples 03 Sum Random number

--- Examples03_Sum/Program.cs
+++ Examples03_Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int nunberA = 31;
-int numberB = 5;
+﻿int nunberA = new Random().Next(1, 10); //случ.числа 1 2 3 4 ... 9
+Console.WriteLine(nunberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB); 
 int result = nunberA + numberB;
 Console.WriteLine(result);
